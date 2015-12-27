65e04aaa755d802715e68e1a3e219fd2efd5d695
iamneha <inehayadav28@gmail.com>
2015-12-28 01:29:43 +0530

    some text added

--- guess_no.py
+++ guess_no.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import random
 guess = 0
+print "I am thinking number between 1 to 6"
 a =random.randint(1,6)
 
 while (guess < 3):
+	print "You have only three chances to guess the number"
 	b = input("Take a guess");
   	guess = guess + 1
 
